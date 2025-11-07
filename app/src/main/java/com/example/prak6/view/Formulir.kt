12a781909b90f514e047ab8f99d31848eb3b5bc0
Ntesaa24 <notesa.aldinasari24@gmail.com>
2025-11-07 10:21:10 +0700

    membuat konstanta jenis kelamin dan submit

--- app/src/main/java/com/example/prak6/view/Formulir.kt
+++ app/src/main/java/com/example/prak6/view/Formulir.kt
@@ -29,3 +29,7 @@ import com.example.prak6.R
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
+fun FormIsian(
+    jenisK: List<String> = listOf("Laki-laki" , "Perempuan"),
+    OnSubmitBtnClick : () -> Unit
+)
